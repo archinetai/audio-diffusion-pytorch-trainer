74c35956d17a9a5cff085a3e6c441168238f433a
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-09-01 14:17:37 +0200

    feat: add EMA, InverseLR schedule, improved logging

--- main/module_upsampler.py
+++ main/module_upsampler.py
@@ -1,4 +1,6 @@
-from typing import Any, Callable, List, Optional
+import warnings
+from copy import deepcopy
+from typing import Any, Callable, Dict, List, Optional, Union
 
 import librosa
 import plotly.graph_objs as go
@@ -11,7 +13,8 @@ from audio_diffusion_pytorch import AudioDiffusionUpsampler, Sampler, Schedule
 from einops import rearrange
 from pytorch_lightning import Callback, Trainer
 from pytorch_lightning.loggers import LoggerCollection, WandbLogger
-from torch import Tensor
+from pytorch_lightning.utilities import rank_zero_only
+from torch import Tensor, optim
 from torch.utils.data import DataLoader
 
 """ Model """
@@ -19,12 +22,23 @@ from torch.utils.data import DataLoader
 
 class Model(pl.LightningModule):
     def __init__(
-        self, learning_rate: float, beta1: float, beta2: float, *args, **kwargs
+        self,
+        lr: float,
+        lr_beta1: float,
+        lr_beta2: float,
+        scheduler_inv_gamma: float,
+        scheduler_power: float,
+        scheduler_warmup: float,
+        *args,
+        **kwargs,
     ):
         super().__init__()
-        self.learning_rate = learning_rate
-        self.beta1 = beta1
-        self.beta2 = beta2
+        self.lr = lr
+        self.lr_beta1 = lr_beta1
+        self.lr_beta2 = lr_beta2
+        self.scheduler_inv_gamma = scheduler_inv_gamma
+        self.scheduler_power = scheduler_power
+        self.scheduler_warmup = scheduler_warmup
         self.model = AudioDiffusionUpsampler(*args, **kwargs)
 
     @property
@@ -34,10 +48,16 @@ class Model(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
             list(self.parameters()),
-            lr=self.learning_rate,
-            betas=(self.beta1, self.beta2),
+            lr=self.lr,
+            betas=(self.lr_beta1, self.lr_beta2),
         )
-        return optimizer
+        scheduler = InverseLR(
+            optimizer=optimizer,
+            inv_gamma=self.scheduler_inv_gamma,
+            power=self.scheduler_power,
+            warmup=self.scheduler_warmup,
+        )
+        return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_idx):
         waveforms = batch
@@ -117,14 +137,15 @@ def get_wandb_logger(trainer: Trainer) -> Optional[WandbLogger]:
 
 
 def log_wandb_audio_batch(
-    logger: WandbLogger, samples: Tensor, sampling_rate: int, comment: str
+    logger: WandbLogger, id: str, samples: Tensor, sampling_rate: int, caption: str = ""
 ):
     num_items = samples.shape[0]
     samples = rearrange(samples, "b c t -> b t c").detach().cpu().numpy()
     logger.log(
         {
-            f"sample_{idx}_{comment}": wandb.Audio(
+            f"sample_{idx}_{id}": wandb.Audio(
                 samples[idx],
+                caption=caption,
                 sample_rate=sampling_rate,
             )
             for idx in range(num_items)
@@ -133,7 +154,7 @@ def log_wandb_audio_batch(
 
 
 def log_wandb_audio_spectrogram(
-    logger: WandbLogger, samples: Tensor, sampling_rate: int, comment: str
+    logger: WandbLogger, id: str, samples: Tensor, sampling_rate: int, caption: str = ""
 ):
     num_items = samples.shape[0]
     samples = samples.detach().cpu()
@@ -151,14 +172,17 @@ def log_wandb_audio_spectrogram(
         image = librosa.power_to_db(spectrogram)
         trace = [go.Heatmap(z=image, colorscale="viridis")]
         layout = go.Layout(
-            yaxis=dict(title="Mel Bin (Log Frequency)"), xaxis=dict(title="Frame")
+            yaxis=dict(title="Mel Bin (Log Frequency)"),
+            xaxis=dict(title="Frame"),
+            title_text=caption,
+            title_font_size=10,
         )
         fig = go.Figure(data=trace, layout=layout)
         return fig
 
     logger.log(
         {
-            f"mel_spectrogram_{idx}_{comment}": get_spectrogram_image(samples[idx])
+            f"mel_spectrogram_{idx}_{id}": get_spectrogram_image(samples[idx])
             for idx in range(num_items)
         }
     )
@@ -210,15 +234,15 @@ class SampleLogger(Callback):
         waveforms = batch[0 : self.num_items]
         log_wandb_audio_batch(
             logger=wandb_logger,
+            id="true",
             samples=waveforms,
             sampling_rate=self.sampling_rate,
-            comment="true",
         )
         log_wandb_audio_spectrogram(
             logger=wandb_logger,
+            id="true",
             samples=waveforms,
             sampling_rate=self.sampling_rate,
-            comment="true",
         )
 
         # Log downsampled waveforms
@@ -226,9 +250,9 @@ class SampleLogger(Callback):
         waveforms_downsampled = waveforms[:, :, :: self.factor]
         log_wandb_audio_batch(
             logger=wandb_logger,
+            id="downsampled",
             samples=waveforms_downsampled,
             sampling_rate=downsampled_rate,
-            comment="downsampled",
         )
 
         # Log upsampled waveforms at different steps
@@ -241,16 +265,196 @@ class SampleLogger(Callback):
             )
             log_wandb_audio_batch(
                 logger=wandb_logger,
+                id="upsampled",
                 samples=samples,
                 sampling_rate=self.sampling_rate,
-                comment="upsampled",
+                caption=f"Sampled in {steps} steps",
             )
             log_wandb_audio_spectrogram(
                 logger=wandb_logger,
+                id="upsampled",
                 samples=samples,
                 sampling_rate=self.sampling_rate,
-                comment="upsampled",
+                caption=f"Sampled in {steps} steps",
             )
 
         if is_train:
             pl_module.train()
+
+
+class EMA(pl.Callback):
+    """Implements EMA (exponential moving average) to any kind of model.
+    EMA weights will be used during validation and stored separately from original model weights.
+
+    How to use EMA:
+        - Sometimes, last EMA checkpoint isn't the best as EMA weights metrics can show long oscillations in time. See
+          https://github.com/rwightman/pytorch-image-models/issues/102
+        - Batch Norm layers and likely any other type of norm layers doesn't need to be updated at the end. See
+          discussions in: https://github.com/rwightman/pytorch-image-models/issues/106#issuecomment-609461088 and
+          https://github.com/rwightman/pytorch-image-models/issues/224
+        - For object detection, SWA usually works better. See   https://github.com/timgaripov/swa/issues/16
+
+    Implementation detail:
+        - See EMA in Pytorch Lightning: https://github.com/PyTorchLightning/pytorch-lightning/issues/10914
+        - When multi gpu, we broadcast ema weights and the original weights in order to only hold 1 copy in memory.
+          This is specially relevant when storing EMA weights on CPU + pinned memory as pinned memory is a limited
+          resource. In addition, we want to avoid duplicated operations in ranks != 0 to reduce jitter and improve
+          performance.
+    """
+
+    def __init__(
+        self,
+        decay: float = 0.9999,
+        update_after_n_steps: int = 100,
+        update_every_n_steps: int = 10,
+        pin_memory=True,
+    ):
+        super().__init__()
+        self.decay = decay
+        self.update_after_n_steps = update_after_n_steps
+        self.update_every_n_steps = update_every_n_steps
+        self.ema_state_dict: Dict[str, torch.Tensor] = {}
+        self.original_state_dict: Dict = {}
+        self._ema_state_dict_ready = False
+
+    @staticmethod
+    def get_state_dict(pl_module: pl.LightningModule):
+        """Returns state dictionary from pl_module. Override if you want filter some parameters and/or buffers out.
+        For example, in pl_module has metrics, you don't want to return their parameters.
+
+        code:
+            # Only consider modules that can be seen by optimizers. Lightning modules can have others nn.Module attached
+            # like losses, metrics, etc.
+            patterns_to_ignore = ("metrics1", "metrics2")
+            return dict(filter(lambda i: i[0].startswith(patterns), pl_module.state_dict().items()))
+        """
+        return pl_module.state_dict()
+
+    @rank_zero_only
+    def on_train_batch_end(
+        self, trainer: "pl.Trainer", pl_module: pl.LightningModule, *args, **kwargs
+    ) -> None:
+        n_step = trainer.global_step
+
+        if (
+            n_step >= self.update_after_n_steps
+            and not self._ema_state_dict_ready
+            and pl_module.global_rank == 0
+        ):
+            # Initialize EMA state dict
+            self.ema_state_dict = deepcopy(self.get_state_dict(pl_module))
+            self._ema_state_dict_ready = True
+
+        if self._ema_state_dict_ready and n_step % self.update_every_n_steps == 0:
+            # Update EMA weights
+            with torch.no_grad():
+                for key, value in self.get_state_dict(pl_module).items():
+                    ema_value = self.ema_state_dict[key]
+                    ema_value.copy_(
+                        self.decay * ema_value + (1.0 - self.decay) * value,
+                        non_blocking=True,
+                    )
+
+    def on_validation_start(
+        self, trainer: pl.Trainer, pl_module: pl.LightningModule
+    ) -> None:
+        if not self._ema_state_dict_ready:
+            return  # Skip Lightning sanity validation check if no ema weights has been loaded from a checkpoint.
+
+        self.original_state_dict = deepcopy(self.get_state_dict(pl_module))
+        pl_module.trainer.training_type_plugin.broadcast(self.ema_state_dict, 0)
+        assert self.ema_state_dict.keys() == self.original_state_dict.keys(), (
+            "There are some keys missing in the ema static dictionary broadcasted. "
+            f"They are: {self.original_state_dict.keys() - self.ema_state_dict.keys()}"
+        )
+        pl_module.load_state_dict(self.ema_state_dict, strict=False)
+
+        if pl_module.global_rank > 0:
+            # Remove ema state dict from the memory. In rank 0, it could be in ram pinned memory.
+            self.ema_state_dict = {}
+
+    def on_validation_end(
+        self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
+    ) -> None:
+        if not self._ema_state_dict_ready:
+            return  # Skip Lightning sanity validation check if no ema weights has been loaded from a checkpoint.
+
+        # Replace EMA weights with training weights
+        pl_module.load_state_dict(self.original_state_dict, strict=False)
+
+    def on_save_checkpoint(
+        self,
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule",
+        checkpoint: Dict[str, Any],
+    ) -> dict:
+        return {
+            "ema_state_dict": self.ema_state_dict,
+            "_ema_state_dict_ready": self._ema_state_dict_ready,
+        }
+
+    def on_load_checkpoint(
+        self,
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule",
+        callback_state: Dict[str, Any],
+    ) -> None:
+        self._ema_state_dict_ready = callback_state["_ema_state_dict_ready"]
+        self.ema_state_dict = callback_state["ema_state_dict"]
+
+
+""" Schedulers """
+
+
+class InverseLR(optim.lr_scheduler._LRScheduler):
+    """
+    From https://github.com/crowsonkb/k-diffusion/blob/master/k_diffusion/utils.py
+
+    Implements an inverse decay learning rate schedule with an optional exponential
+    warmup. When last_epoch=-1, sets initial lr as lr.
+    inv_gamma is the number of steps/epochs required for the learning rate to decay to
+    (1 / 2)**power of its original value.
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        inv_gamma (float): Inverse multiplicative factor of learning rate decay. Default: 1.
+        power (float): Exponential factor of learning rate decay. Default: 1.
+        warmup (float): Exponential warmup factor (0 <= warmup < 1, 0 to disable)
+            Default: 0.
+        min_lr (float): The minimum learning rate. Default: 0.
+        last_epoch (int): The index of last epoch. Default: -1.
+        verbose (bool): If ``True``, prints a message to stdout for
+            each update. Default: ``False``.
+    """
+
+    def __init__(
+        self,
+        optimizer,
+        inv_gamma: float = 1.0,
+        power: float = 1.0,
+        warmup: float = 0.0,
+        min_lr: float = 0.0,
+        *args,
+        **kwargs,
+    ):
+        self.inv_gamma = inv_gamma
+        self.power = power
+        if not 0.0 <= warmup < 1:
+            raise ValueError("Invalid value for warmup")
+        self.warmup = warmup
+        self.min_lr = min_lr
+        super().__init__(optimizer, *args, **kwargs)
+
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            warnings.warn(
+                "To get the last learning rate computed by the scheduler, "
+                "please use `get_last_lr()`."
+            )
+        return self._get_closed_form_lr()
+
+    def _get_closed_form_lr(self):
+        warmup = 1 - self.warmup ** (self.last_epoch + 1)
+        lr_mult = (1 + self.last_epoch / self.inv_gamma) ** -self.power
+        return [
+            warmup * max(self.min_lr, base_lr * lr_mult) for base_lr in self.base_lrs
+        ]
