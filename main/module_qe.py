e6d3d976d09d205744635ad3340642e936db970f
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-09-27 10:11:21 +0200

    feat: back to snd loss

--- main/module_qe.py
+++ main/module_qe.py
@@ -41,14 +41,14 @@ class Model(pl.LightningModule):
         self.quantizer_loss_weight = quantizer_loss_weight
 
         self.autoencoder = autoencoder
-        # self.loss_fn = auraloss.freq.SumAndDifferenceSTFTLoss()
         self.loss_fn = None
+        self.loss_fn = auraloss.freq.SumAndDifferenceSTFTLoss()
 
     def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
-        if self.loss_fn is None:
-            self.loss_fn = auraloss.freq.MultiResolutionSTFTLoss(
-                scale="mel", n_bins=64, sample_rate=self.sample_rate, device=self.device
-            )
+        # if self.loss_fn is None:
+        #     self.loss_fn = auraloss.freq.MultiResolutionSTFTLoss(
+        #         scale="mel", n_bins=64, sample_rate=self.sample_rate, device=self.device
+        #     )
         z, info = self.autoencoder.encode(x, with_info=True)  # type: ignore
         y = self.autoencoder.decode(z)  # type: ignore
         loss = self.loss_fn(x, y)  # type: ignore
