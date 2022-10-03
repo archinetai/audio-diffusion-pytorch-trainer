4bf37777997bd69735802a7425c51242f28dfd57
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-10-03 13:10:59 +0200

    feat: add qe_ar with a-transformers

---
+++ main/module_qe_ar2.py
@@ -0,0 +1,289 @@
+from functools import reduce
+from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple
+
+import librosa
+import plotly.graph_objects as go
+import plotly.graph_objs as go
+import pytorch_lightning as pl
+import torch
+import torchaudio
+import wandb
+from audio_data_pytorch.utils import fractional_random_split
+from audio_diffusion_pytorch import AudioDiffusionAutoencoder, Sampler, Schedule
+from einops import rearrange
+from pytorch_lightning import Callback, Trainer
+from pytorch_lightning.loggers import LoggerCollection, WandbLogger
+from torch import LongTensor, Tensor, nn
+from torch.utils.data import DataLoader
+from transformers import GPT2Config, GPT2LMHeadModel
+
+""" Model """
+
+
+class Model(pl.LightningModule):
+    def __init__(
+        self,
+        lr: float,
+        lr_eps: float,
+        lr_beta1: float,
+        lr_beta2: float,
+        lr_weight_decay: float,
+        transformer: nn.Module,
+        autoencoder_path: str,  # Must be quantized
+        codebook_size: int,
+        num_residuals: int,
+        shared_codebook: bool,
+    ):
+        super().__init__()
+        self.lr = lr
+        self.lr_eps = lr_eps
+        self.lr_beta1 = lr_beta1
+        self.lr_beta2 = lr_beta2
+        self.lr_weight_decay = lr_weight_decay
+
+        self.num_residuals = num_residuals
+        self.shared_codebook = shared_codebook
+        self.codebook_size = codebook_size
+
+        self.transformer = transformer
+        self.autoencoder = torch.load(autoencoder_path, map_location=self.device)
+
+    @torch.no_grad()
+    def encode(self, x: Tensor) -> Tensor:
+        context, info = self.autoencoder.encode(x, with_info=True)
+        # Encode indices sequentially
+        indices = info["indices"]
+        if not self.shared_codebook:
+            for i in range(1, self.num_residuals):
+                indices[:, :, :, i] = indices[:, :, :, i] + self.codebook_size * i
+        indices = rearrange(indices, "b 1 k r -> b (k r)")  # b n
+        return indices
+
+    @torch.no_grad()
+    def decode(self, indices: Tensor) -> Tensor:
+        indices = rearrange(indices, "b (k r) -> b 1 k r", r=self.num_residuals)
+        if not self.shared_codebook:
+            for i in range(1, self.num_residuals):
+                indices[:, :, :, i] = indices[:, :, :, i] - self.codebook_size * i
+        indices = indices.clamp(0, self.codebook_size - 1)
+        latent = self.autoencoder.bottleneck.from_ids(indices)
+        return self.autoencoder.decode(latent)
+
+    def forward(self, x: Tensor) -> Tensor:
+        indices = self.encode(x)
+        return self.transformer(indices)
+
+    def training_step(self, batch, batch_idx):
+        waveforms = batch
+        loss = self(waveforms)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        waveforms = batch
+        loss = self(waveforms)
+        self.log("valid_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(
+            list(self.transformer.parameters()),
+            lr=self.lr,
+            betas=(self.lr_beta1, self.lr_beta2),
+            eps=self.lr_eps,
+            # weight_decay=self.lr_weight_decay,
+        )
+        return optimizer
+
+    @property
+    def device(self):
+        return next(self.transformer.parameters()).device
+
+
+""" Datamodule """
+
+
+class Datamodule(pl.LightningDataModule):
+    def __init__(
+        self,
+        dataset,
+        *,
+        val_split: float,
+        batch_size: int,
+        num_workers: int,
+        pin_memory: bool = False,
+        **kwargs: int,
+    ) -> None:
+        super().__init__()
+        self.dataset = dataset
+        self.val_split = val_split
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+        self.data_train: Any = None
+        self.data_val: Any = None
+
+    def setup(self, stage: Any = None) -> None:
+        split = [1.0 - self.val_split, self.val_split]
+        self.data_train, self.data_val = fractional_random_split(self.dataset, split)
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=True,
+        )
+
+
+""" Callbacks """
+
+
+def get_wandb_logger(trainer: Trainer) -> Optional[WandbLogger]:
+    """Safely get Weights&Biases logger from Trainer."""
+
+    if isinstance(trainer.logger, WandbLogger):
+        return trainer.logger
+
+    if isinstance(trainer.logger, LoggerCollection):
+        for logger in trainer.logger:
+            if isinstance(logger, WandbLogger):
+                return logger
+
+    print("WandbLogger not found.")
+    return None
+
+
+def log_wandb_audio_batch(
+    logger: WandbLogger, id: str, samples: Tensor, sampling_rate: int, caption: str = ""
+):
+    num_items = samples.shape[0]
+    samples = rearrange(samples, "b c t -> b t c").detach().cpu().numpy()
+    logger.log(
+        {
+            f"sample_{idx}_{id}": wandb.Audio(
+                samples[idx],
+                caption=caption,
+                sample_rate=sampling_rate,
+            )
+            for idx in range(num_items)
+        }
+    )
+
+
+def log_wandb_audio_spectrogram(
+    logger: WandbLogger, id: str, samples: Tensor, sampling_rate: int, caption: str = ""
+):
+    num_items = samples.shape[0]
+    samples = samples.detach().cpu()
+    transform = torchaudio.transforms.MelSpectrogram(
+        sample_rate=sampling_rate,
+        n_fft=1024,
+        hop_length=512,
+        n_mels=80,
+        center=True,
+        norm="slaney",
+    )
+
+    def get_spectrogram_image(x):
+        spectrogram = transform(x[0])
+        image = librosa.power_to_db(spectrogram)
+        trace = [go.Heatmap(z=image, colorscale="viridis")]
+        layout = go.Layout(
+            yaxis=dict(title="Mel Bin (Log Frequency)"),
+            xaxis=dict(title="Frame"),
+            title_text=caption,
+            title_font_size=10,
+        )
+        fig = go.Figure(data=trace, layout=layout)
+        return fig
+
+    logger.log(
+        {
+            f"mel_spectrogram_{idx}_{id}": get_spectrogram_image(samples[idx])
+            for idx in range(num_items)
+        }
+    )
+
+
+class SampleLogger(Callback):
+    def __init__(
+        self, num_items: int, channels: int, sampling_rate: int, length: int
+    ) -> None:
+        self.num_items = num_items
+        self.channels = channels
+        self.sampling_rate = sampling_rate
+        self.length = length
+        self.epoch_count = 0
+
+        self.log_next = False
+
+    def on_validation_epoch_start(self, trainer, pl_module):
+        self.log_next = True
+
+    def on_validation_batch_start(
+        self, trainer, pl_module, batch, batch_idx, dataloader_idx
+    ):
+        if self.log_next:
+            self.log_sample(trainer, pl_module, batch)
+            self.log_next = False
+
+    @torch.no_grad()
+    def log_sample(self, trainer, pl_module, batch):
+        is_train = pl_module.training
+        if is_train:
+            pl_module.eval()
+
+        wandb_logger = get_wandb_logger(trainer).experiment
+
+        # Encode x_true to get indices
+        x_true = batch[0 : self.num_items]
+
+        log_wandb_audio_batch(
+            logger=wandb_logger,
+            id="true",
+            samples=x_true,
+            sampling_rate=self.sampling_rate,
+        )
+        log_wandb_audio_spectrogram(
+            logger=wandb_logger,
+            id="true",
+            samples=x_true,
+            sampling_rate=self.sampling_rate,
+        )
+
+        indices = pl_module.encode(x_true)
+
+        length = indices.shape[-1]
+        indices_generated = pl_module.transformer.generate(
+            indices,
+            sequence_length=length,  # Autoregressively predict another full sequence
+        )
+
+        samples = pl_module.decode(indices_generated)
+
+        log_wandb_audio_batch(
+            logger=wandb_logger,
+            id="recon",
+            samples=samples,
+            sampling_rate=self.sampling_rate,
+        )
+        log_wandb_audio_spectrogram(
+            logger=wandb_logger,
+            id="recon",
+            samples=samples,
+            sampling_rate=self.sampling_rate,
+        )
+
+        if is_train:
+            pl_module.train()
